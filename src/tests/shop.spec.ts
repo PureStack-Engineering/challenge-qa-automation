2e22c0a0c43bf37f3d7ef4ba8fe7697520b32dee
José Luis Mora Castilla <Jlmoracastilla@gmail.com>
2025-12-13 18:51:42 +0100

    Create shop.spec.ts

---
+++ src/tests/shop.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+// TODO: Import your Page Objects here
+
+test.describe('SauceDemo Shopping Flow', () => {
+  
+  test('should allow a user to buy a backpack', async ({ page }) => {
+    // TODO: Implement the test using Page Object Model
+    // 1. Login
+    // 2. Add Backpack
+    // 3. Verify Price in Cart
+    
+    // Hint: await loginPage.login('standard_user', 'secret_sauce');
+  });
+
+});
