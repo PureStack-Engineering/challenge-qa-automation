dee93e4f34294981125926cab055227c7127c103
José Luis Mora Castilla <Jlmoracastilla@gmail.com>
2026-01-28 09:40:50 +0100

    Update shop.spec.ts

--- src/tests/shop.spec.ts
+++ src/tests/shop.spec.ts
@@ -2,10 +2,9 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
 test('Sanity Check: Swag Labs loads correctly', async ({ page }) => {
-    // This test ensures the environment is ready.
+  
     const login = new LoginPage(page);
     await login.goto();
     
-    // Check if the title is correct
     await expect(page).toHaveTitle(/Swag Labs/);
 });
