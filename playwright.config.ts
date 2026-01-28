2c42d5f595085bfbe0d4c0def53a1a91692e0829
José Luis Mora Castilla <Jlmoracastilla@gmail.com>
2026-01-28 09:39:55 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './src/tests', // 👈 Importante: Apunta a la carpeta que definimos
+  testDir: './src/tests', 
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
