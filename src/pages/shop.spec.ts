9140ed18751cb6373c7466e4e56389cdbfbbac13
José Luis Mora Castilla <Jlmoracastilla@gmail.com>
2026-01-28 09:41:20 +0100

    Update shop.spec.ts

--- src/pages/shop.spec.ts
+++ src/pages/shop.spec.ts
@@ -3,16 +3,14 @@ import { LoginPage } from '../pages/LoginPage';
 import { InventoryPage } from '../pages/InventoryPage';
 
 test('SauceDemo Purchase Flow', async ({ page }) => {
-  // 🏗️ SETUP
+
   const loginPage = new LoginPage(page);
   const inventoryPage = new InventoryPage(page);
 
-  // 🎭 ACT
+ 
   await loginPage.goto();
   await loginPage.login('standard_user', 'secret_sauce');
   
-  // 🧨 Esta aserción fallará inicialmente porque no has implementado la lógica
-  // El candidato debe hacer que esto pase.
   await expect(page).toHaveURL(/inventory.html/);
 
   await inventoryPage.addItemToCart('Sauce Labs Backpack');
